8214e16afb0eb66240fd51cd3b987713ed2bc0ef
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-01-29 12:25:46 +0100

    new nav yet again

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,6 +1,13 @@
-const burger = document.querySelector(".burger-menu");
-const menu = document.querySelector(".menu");
+// const burger = document.querySelector(".burger-menu");
+// const menu = document.querySelector(".menu");
 
-burger.addEventListener("click", () => {
-  menu.classList.toggle("menu-show");
+// burger.addEventListener("click", () => {
+//   menu.classList.toggle("menu-show");
+// });
+
+const menuBtn = document.querySelector(".navbar-toggler");
+const collapsableMenu = document.querySelector(".navbar-collapse");
+
+menuBtn.addEventListener("click", () => {
+  collapsableMenu.classList.toggle("collapse");
 });
